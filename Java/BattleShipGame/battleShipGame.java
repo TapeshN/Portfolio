c55ba50d67055bfca15a5a7deaed73ae35c14eba
Tapesh Nagarwal <tapesh01@gmail.com>
2018-07-28 18:35:52 -0400

    defined methods within the BattleShipGame

--- Java/BattleShipGame/battleShipGame.java
+++ Java/BattleShipGame/battleShipGame.java
@@ -1,13 +1,33 @@
 
 public class BattleShipGame {
 
+	//declare and initialize the variables we'll need
 	private GameHelper helper = new GameHelper();
 	private ArrayList<Ship> shipList = new ArrayList<Shop>();
 	private int numOfGuesses = 0;
 
-	public static void main(String[] args) {
-		
+	//make the game board with 3 ships
+	private void setUpGame(){
+	}
 
+	//initialize game
+	private void startPlaying(){
+	}
+	
+	//method to check user guesses
+	private void checkUserGuess(String userGuess){
+	}
+
+	//method to end and return user feedback
+	private void finishGame(){
+	}
+
+
+	public static void main(String[] args) {
+		BattleShipGame game = new BattleShipGame();
+		game.setUpGame();
+		game.startPlaying();
+	
 	}
 
 }
