d0edf151613597e9c1ea42c4ae8d7cf2fefeceaf
Tapesh Nagarwal <tapesh01@gmail.com>
2018-07-28 18:29:46 -0400

    declare variables

--- Java/BattleShipGame/battleShipGame.java
+++ Java/BattleShipGame/battleShipGame.java
@@ -1,5 +1,9 @@
 
-public class battleShipGame {
+public class BattleShipGame {
+
+	private GameHelper helper = new GameHelper();
+	private ArrayList<Ship> shipList = new ArrayList<Shop>();
+	private int numOfGuesses = 0;
 
 	public static void main(String[] args) {
 		
