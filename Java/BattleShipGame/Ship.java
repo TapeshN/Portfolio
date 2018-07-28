076dc40cd9f8b15bf57578f0399466dcc623d334
Tapesh Nagarwal <tapesh01@gmail.com>
2018-07-28 18:38:05 -0400

    created methods for ship object

--- Java/BattleShipGame/Ship.java
+++ Java/BattleShipGame/Ship.java
@@ -1,4 +1,22 @@
-
+import java.util.*;
 public class Ship {
+	private ArrayList<String> locationCells;
+	private String name;
+
+	public void setLocationCells(ArrayList<String> loc>{
+	}
+
+	public void setName(String n){
+	}
+
+	public String checkYourself(String userInput){
+	}
+
+
+
+
+
+
+
 
 }
