56311f8d9c752d34f0e3492b5a4b27e19cba5e74
Tapesh Nagarwal <tapesh01@gmail.com>
2018-07-29 11:15:22 -0400

    create setup() for game and construct all ship methods

--- Java/BattleShipGame/battleShipGame.java
+++ Java/BattleShipGame/battleShipGame.java
@@ -8,6 +8,29 @@ public class BattleShipGame {
 
 	//make the game board with 3 ships
 	private void setUpGame(){
+		Ship one = new Ship();
+		one.setName("Mayflower");
+		Ship two = new Ship();
+		two.setName("RMS Lusitania");
+		Ship three = new Ship();
+		three.setName("USS Arizona");
+		
+		//add ships to shipList
+		shipList.add(one);
+		shipList.add(two);
+		shipList.add(three);
+
+		//print brief instructions for user
+		System.out.println("Your goal is to sink three battleships");
+		System.out.println("The Mayflower, RMS Lusitania, and the USS Arizona");
+		System.out.println("Try to sink them all in the fewest number of guesses");
+		
+
+		//make locations for all three ships
+		for (Ship currShip : shipList){
+			ArrayList<String> newLocation = helper.placeShip(3);
+			currShip.setLocationCells(newLocation);
+		}
 	}
 
 	//initialize game
