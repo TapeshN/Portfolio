280f7c0852c641875d338a83ef47531044d35968
Tapesh Nagarwal <tapesh01@gmail.com>
2018-07-29 11:24:07 -0400

    Constructed all methods within BattleShipGame class

--- Java/BattleShipGame/battleShipGame.java
+++ Java/BattleShipGame/battleShipGame.java
@@ -33,24 +33,45 @@ public class BattleShipGame {
 		}
 	}
 
-	//initialize game
+	//initialize game and keep asking for user guesses until shipList is all sunk
 	private void startPlaying(){
+		while(!shipList.isEmpty()){	
+			String userGuess = helper.getUserInput("Enter a Guess: ");
+			checkUserGuess(userGuess);
+		}
+		finishGame();
 	}
 	
 	//method to check user guesses
 	private void checkUserGuess(String userGuess){
+		numOfGuesses++;					//increment the number of guesses the user has made
+		String result = "miss";				//assume it is a 'miss' unless told otherwise
+		for(int x = 0; x<shipList.size(); x++){		//for all Ships in shipList
+			result = shipList.get(x).checkYourself(userGuess);	//ask the Ship to check the user guess while looking for hit
+			if (result.equals("hit"){
+				shipList.remove(x);				//if userGuess hit's ship, remove the ship
+				break;
+			}
+		}
+		System.out.println(result);				//print result for user
+	
 	}
 
 	//method to end and return user feedback
 	private void finishGame(){
-	}
+		System.out.println("You've sunk all the ships!");
+		if(numOfGuesses <= 18){
+			System.out.println("It only took you + numOfGuesses + " guesses.");
+			System.out.println("You got out before your options sank.");
+		} else{
+			System.out.println("Took you long enough. " numOfGuesses + " guesses.);
+		}
 
 
 	public static void main(String[] args) {
 		BattleShipGame game = new BattleShipGame();
 		game.setUpGame();
 		game.startPlaying();
-	
 	}
 
 }
